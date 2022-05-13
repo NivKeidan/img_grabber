b3199d1c41837475fe03a231c9c7ec73090d50f8
Niv Keidan <nivkeidan@gmail.com>
2022-05-13 14:31:34 +0300

    bring back linux binary decrease executables file size

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-GOOS=windows GOARCH=amd64 go build -o img_grab_win.exe run.go
+GOOS=linux GOARCH=amd64 go build -ldflags=-w -o img_grab_linux run.go
+GOOS=windows GOARCH=amd64 go build -ldflags=-w -o img_grab_win.exe run.go
